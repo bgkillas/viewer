72c47ff44ed95217ae0b5dd8b4464b4237c3456e
bgkillas <walhelm@proton.me>
2025-03-18 11:08:03 -0400

    display images

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use eframe::emath::pos2;
-use egui::load::TexturePoll;
-use egui::{CentralPanel, Color32, Pos2, Rect, SizeHint, TextureHandle, TextureOptions, Vec2};
+use egui::{CentralPanel, Color32, Pos2, Rect, TextureHandle, TextureOptions, Vec2};
 use eyre::{ContextCompat, eyre};
+use image::ImageReader;
 use std::collections::HashMap;
+use std::fmt::{Display, Formatter};
 use std::path::{Path, PathBuf};
 use std::{env, fs};
 fn main() -> eframe::Result {
-    eframe::run_native(
+    Ok(eframe::run_native(
         "viewer",
         eframe::NativeOptions::default(),
-        Box::new(|_cc| Ok(Box::new(App::new().unwrap()))),
-    )
+        Box::new(|_cc| Ok(Box::new(App::new()?))),
+    )?)
 }
 #[derive(PartialEq, Clone)]
 struct Chapter {
@@ -18,9 +19,9 @@ struct Chapter {
     minor: Option<usize>,
 }
 
-impl Chapter {
-    fn to_string(&self) -> String {
-        format!("{:04}{}", self.major, self.minor.unwrap_or(0))
+impl Display for Chapter {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:04}{}", self.major, self.minor.unwrap_or(0))
     }
 }
 
@@ -28,6 +29,19 @@ impl Chapter {
 struct Page {
     chapter: Chapter,
     page: Option<usize>,
+}
+
+impl Display for Page {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}{}",
+            self.chapter,
+            self.page
+                .map(|p| format!("-{:03}", p))
+                .unwrap_or_default()
+        )
+    }
 }
 
 impl Page {
@@ -49,15 +63,6 @@ impl Page {
         };
         Ok(Page { chapter, page })
     }
-    fn to_string(&self) -> String {
-        format!(
-            "{}{}",
-            self.chapter.to_string(),
-            self.page
-                .map(|p| format!("-{:03}", p))
-                .unwrap_or(String::new())
-        )
-    }
 }
 
 struct App {
@@ -70,10 +75,10 @@ struct App {
 }
 
 impl eframe::App for App {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         CentralPanel::default()
             .frame(egui::Frame::default().fill(Color32::from_rgb(0, 0, 0)))
-            .show(ctx, |ui| self.main(ui));
+            .show(ctx, |ui| self.main(ctx, ui, frame).unwrap());
     }
 }
 impl App {
@@ -90,15 +95,13 @@ impl App {
             Err(eyre!("bad paths"))
         } else {
             let raw = fs::read_to_string(&data)?
-                .trim()
-                .chars()
-                .collect::<String>();
+                .trim().to_string();
             let is_list = !raw.contains('-');
             let current = Page::parse(&raw, is_list)?;
-            let mut images = Default::default();
+            let images = Default::default();
             let mut pages = Vec::new();
             for p in fs::read_dir(&image_path)? {
-                pages.push(Page::parse(p?.path().to_str().unwrap(), is_list)?)
+                pages.push(Page::parse(p?.path().file_name().unwrap().to_str().unwrap(), is_list)?)
             }
             let current = pages
                 .iter()
@@ -114,25 +117,54 @@ impl App {
             })
         }
     }
-    fn main(&mut self, ui: &mut egui::Ui) -> eyre::Result<()> {
-                if !self.images.iter().any(|(p, _)| p == &self.current) {
-                    let img = ImageReader::
-            let color_image =
-                egui::ColorImage::from_rgba_unmultiplied(size, img.as_flat_samples().as_slice());
-            let tex = ui
-                .ctx()
-                .load_texture(name, color_image, TextureOptions::NEAREST);
-            self.images.insert(self.current, tex);
+    fn get_path(&self, page: &Page) -> PathBuf {
+        self.image_path.join(page.to_string())
+    }
+    fn get_img(&mut self, ui: &mut egui::Ui, num: usize) -> eyre::Result<()> {
+        let p = &self.pages[num];
+        let img = ImageReader::open(self.get_path(p))?.with_guessed_format()?.decode()?.to_rgb8();
+        let color_image = egui::ColorImage::from_rgb(
+            [img.width() as usize, img.height() as usize],
+            img.as_flat_samples().as_slice(),
+        );
+        let tex = ui
+            .ctx()
+            .load_texture(p.to_string(), color_image, TextureOptions::NEAREST);
+        self.images.insert(num, tex);
+        Ok(())
+    }
+    fn main(&mut self, ctx: &egui::Context, ui: &mut egui::Ui, frame: &mut eframe::Frame) -> eyre::Result<()> {
+        {
+            let range = self.current.saturating_sub(1)..(self.current + 2).min(self.pages.len());
+            let mut to_remove = Vec::new();
+            for i in self.images.keys() {
+                if !range.contains(i) {
+                    to_remove.push(*i);
+                    ctx.forget_image(&self.pages[*i].to_string());
+                }
+            }
+            for i in to_remove {
+                self.images.remove(&i);
+            }
+            for i in range {
+                if !self.images.contains_key(&i) {
+                    self.get_img(ui, i)?
+                }
+            }
         }
-                let painter = ui.painter();
-                let image = self.images.get(&self.current).unwrap();
-                let rect = Rect::from_min_size(Pos2::new(0.0, 0.0), image.size().unwrap());
-                painter.image(
-                    image.texture_id().unwrap(),
-                    rect,
-                    Rect::from_min_max(pos2(0.0, 0.0), pos2(1.0, 1.0)),
-                    Color32::BLACK,
-                );
+        let painter = ui.painter();
+        let image = self.images.get(&self.current).unwrap();
+        let size = image.size();
+        let rect = Rect::from_min_size(
+            Pos2::new(ctx.input(|i| i.screen_rect).width() / 2.0 - size[0] as f32 / 2.0, 0.0),
+            Vec2::new(size[0] as f32, size[1] as f32),
+        );
+        painter.image(
+            image.id(),
+            rect,
+            Rect::from_min_max(pos2(0.0, 0.0), pos2(1.0, 1.0)),
+            Color32::WHITE,
+        );
         Ok(())
     }
 }
